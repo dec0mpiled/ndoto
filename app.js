97894ed2271c2ad02b33a94ccdc4abeab8e4fb33
dec0mpiled <liimbix@gmail.com>
2016-01-14 17:17:49 +0000

    ndoto

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error('Page or File not found on nDoto!');
   err.status = 404;
   next(err);
 });
