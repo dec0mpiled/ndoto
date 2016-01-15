0f1d8925580fee5d906412167fc8c4f4044456bb
dec0mpiled <liimbix@gmail.com>
2016-01-15 19:15:51 +0000

    ndoto

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var app = express();
+var email = require('emailjs')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,6 +19,29 @@ router.get('/credits', function(req, res, next) {
   res.render('credits', { title: 'Credits' });
 });
 
+/*mail*/
+router.post('/sendMail', function(req, res, next) {
+  var texta = req.body.bug;
+  var gnameoruser = req.body.nameoruser;
+  var gbugemail = req.body.bugemail;
+  var server = email.server.connect({
+  user: 'ndotodrew@gmail.com',
+  password: 'welcometor4ge!',
+  host: 'smtp.gmail.com',
+  ssl: true
+});
+
+server.send({
+  text: texta.toString(),
+  from: gbugemail+" - nDoto.co:",
+  to: 'Drew Tarnowski <ndotodrew@gmail.com>',
+  cc: '',
+  subject: 'A Bug Report / Feedback from nDoto.co has been submitted by '+gnameoruser
+}, function (err, message) {
+  console.log(err || message);
+});
+})
+
 /* GET RegisterSubmit*/
 // document is not defined
 // you have to use POST
@@ -32,13 +56,25 @@ router.post('/regsubmit', function(req, res, next) {
   // req.body.(name of item)
    var username = req.body.username;
    var password = req.body.password;
-   
-  
-  
- fs.appendFile('users.csv', username + ': {' + username + ',' + password + '}' + '\n', function(err) {
+
+    fs.readFile('registerednames.csv', 'utf-8', function (err, reada) {
+    if (err) throw err;
+    console.log(reada);
+    var newtext = reada.toString();
+    var buscar = newtext.search(username);
+    console.log(buscar);
+    if (buscar>-1) {
+      res.render('login', {
+        err: 'name in use!'
+      });
+    }
+    
+  else {
+ fs.appendFile('users.csv', '%' + username + '%: {' + username + ',' + password + '}' + '\n', function(err) {
     fs.appendFile('registerednames.csv', username + '\n')
    var read = fs.readFile('users.csv', 'utf-8', function (err, read) {
     read.toString();
+    console.log(read);
     /*for (i=0; i < read.length; i++) {
       var currentchar= read.charAt(i);
       console.log(currentchar);
@@ -69,6 +105,8 @@ router.post('/regsubmit', function(req, res, next) {
     if (err) throw err;
     
   });
+};
+});
 });
 
 /* GET regisdone page */
@@ -82,7 +120,13 @@ router.get('/login', function(req, res, next) {
 });
 
 router.get('/bugreport', function(req, res, next) {
-  res.render('bugreport', { title: 'Report a Bug' });
+  res.render('bugreport', { title: 'Report a Bug / Send Feedback' });
+});
+
+router.get('/admin*:key', function(req, res, next) {
+  if (req.params.key=='tar*112715'){
+  res.render('adminpriv', { title: 'Admin Console' });
+  }
 });
 
 router.get('/addLike', function(req, res, next) {
